43e6e14cf02b233f779ec70d9d40bd60ed2ce87c
Indy Kroonen <indykroonen@gmail.com>
2019-09-09 14:39:07 +0200

    LAb 1
    klaar

---
+++ LABS/Lab 1/js/b1w2l1.js
@@ -0,0 +1,9 @@
+// begin hier je JavaScript commandos
+var voornaam = prompt("'Wat is uw voornaam?");
+var achternaam = prompt("Wat is uw achternaam?"); 
+var leeftijd = prompt("Wat is uw leeftijd"); 
+
+document.write("Voornaam: " + voornaam + "<br>"); 
+document.write("Achternaam: " + achternaam +"<br>");
+document.write("Leeftijd: " + leeftijd + "<br>"); 
+
